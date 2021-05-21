bad0cff96e5244f16e694795294cb7cee19e51a7
Isadora Kondo <kondoiqcdev@gmail.com>
2021-05-21 19:35:32 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace calculadoraidade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime nascimento = new DateTime(2001, 9, 23);
+            DateTime agora = new DateTime(2021, 5, 21);
+            TimeSpan vida = nascimento-agora;
+            int vivi = vida.Days;
+            Console.Write("Eu nasci no dia "+nascimento);
+            Console.Write(" e já vivi "+vivi+" dias.");
+            
+        }
+    }
+}
